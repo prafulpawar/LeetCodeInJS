453ee37aac907c2f561591502dfee9f24ba1424c
prafulpawar <prafuldukhi@gmail.com>
2025-01-02 12:12:32 +0530

    "Comited"

--- 2DArr/HardCoded.js
+++ 2DArr/HardCoded.js
@@ -5,7 +5,7 @@ let arr = [
 ]
 for(let i=0;i<arr.length;i++){
     for(let j=0;j<arr[i].length;j++){
-        console.log(arr[i] [j])
+        console.log(arr[i]  [j] )
     }
     console.log('\n');
 }
---
+++ 2DArr/Input2d.js
@@ -0,0 +1,31 @@
+let prompt = require('prompt-sync')();
+let row = Number(prompt('Row Size Is: '));
+let col = Number(prompt('Col Size Is: '));
+
+let arr = Array.from({ length: row }, () => Array(col));
+
+
+for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr[i].length;j++){
+        arr[i][j] = Number(prompt(`Enter element at (${i}, ${j}):`));
+    }
+}
+
+console.log(arr); // Prints the 2D array
+
+
+
+
+// let rows = parseInt(prompt("Enter number of rows:"));
+// let cols = parseInt(prompt("Enter number of columns:"));
+
+// // Create 2D array
+// let arr = [];
+// for (let i = 0; i < rows; i++) {
+//   arr[i] = []; // Initialize each row
+//   for (let j = 0; j < cols; j++) {
+//     arr[i][j] = parseInt(prompt(`Enter element at (${i}, ${j}):`)); // Input values
+//   }
+// }
+
+// console.log(arr); // Prints the 2D array
