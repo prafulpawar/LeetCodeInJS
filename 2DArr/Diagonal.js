d896c7fae0f8a4c1f07f52bcf40469a486858d9f
prafulpawar <prafuldukhi@gmail.com>
2025-01-02 12:41:15 +0530

    "Done"

--- 2DArr/Diagonal.js
+++ 2DArr/Diagonal.js
@@ -4,12 +4,12 @@ let col = Number(prompt('Col Size Is: '));
 
 let arr = Array.from({ length: row }, () => Array(col));
 
-
 for(let i=0;i<arr.length;i++){
     for(let j=0;j<arr[i].length;j++){
         arr[i][j] = Number(prompt(`Enter element at (${i}, ${j}):`));
     }
 }
+
 let leftSum=0;
 let rightSum=0;
 for(let i=0;i<arr.length;i++){
