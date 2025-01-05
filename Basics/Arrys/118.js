1cf152e59cf08a8d24d41e391d63f52091eb285d
prafulpawar <prafuldukhi@gmail.com>
2025-01-06 00:34:08 +0530

    "Done"

--- Basics/Arrys/118.js
+++ Basics/Arrys/118.js
@@ -5,16 +5,18 @@
  */
 var generate = function (numRows) {
     const arr = [];
-    const rows = 3;
+     numRows = 3;
 
     // Dynamically filling rows and columns
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < numRows; i++) {
         const cols = i + 1; // Number of columns increases with each row
         arr[i] = [];
         for (let j = 0; j < cols; j++) {
+            
             arr[i][j] = (i + 1) * (j + 1);  // Filling with some values
         }
     }
+    return arr
 };
 
 let numRows = 5
