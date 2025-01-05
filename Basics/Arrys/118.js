f05126202aaa172477642f640dfeb506f9ea3980
prafulpawar <prafuldukhi@gmail.com>
2025-01-06 00:42:38 +0530

    "Done"

--- Basics/Arrys/118.js
+++ Basics/Arrys/118.js
@@ -3,6 +3,26 @@
  * @param {number} numRows
  * @return {number[][]}
  */
+
+function fact (){
+    
+  function calculateFactorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+
+  
+  let factNumRows =  calculateFactorial(numRows);
+  let factCol = calculateFactorial(col);
+  
+  
+}
 var generate = function (numRows) {
     const arr = [];
      numRows = 3;
@@ -12,6 +32,7 @@ var generate = function (numRows) {
         const cols = i + 1; // Number of columns increases with each row
         arr[i] = [];
         for (let j = 0; j < cols; j++) {
+               fact=(numRows,col)
             arr[i][j] = (i + 1) * (j + 1);  // Filling with some values
         }
     }
