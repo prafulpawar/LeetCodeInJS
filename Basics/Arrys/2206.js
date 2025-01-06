57837c391444db24b609b9c1db470e1526fc5a89
prafulpawar <prafuldukhi@gmail.com>
2025-01-06 14:23:44 +0530

    "Done"

--- Basics/Arrys/2206.js
+++ Basics/Arrys/2206.js
@@ -1,9 +1,16 @@
 var divideArray = function(nums) {
-    let len = nums.length;
     nums = nums.sort();
-    let pairs=Math.floor(nums/2);
-
+    let flag = false;
+    let k=1
+    for(let i=0; i<nums.length;i=i+2){
+        if(nums[i] !== nums[i+1] ){
+            return flag;
+        }
+    }
+    flag = true;
+    return flag;  
 };
 
-let nums = [3,2,3,2,2,2]; 
+let nums = [9,4,18,3,2,6,18,15,7,15,6,4,15,14,7,4,15,4,3,17,9,13,13,12,2,14,12,17]; 
 let x  = divideArray(nums)
+console.log(x);
