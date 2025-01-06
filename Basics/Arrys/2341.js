ff1e340cfdc52627e3ae7a257b2828652a58402b
prafulpawar <prafuldukhi@gmail.com>
2025-01-06 14:57:32 +0530

    "Done"

--- Basics/Arrys/2341.js
+++ Basics/Arrys/2341.js
@@ -3,5 +3,24 @@
  * @return {number[]}
  */
 var numberOfPairs = function(nums) {
-    
+    nums = nums.sort()  
+    let count =0;
+    let pair = 0;
+
+    for(let i=0; i<nums.length;i=k){
+        if(nums[i] !== nums[i+1] ){
+            k=i+1;
+            count++;
+        }
+        else{
+            pair++
+            k=i+2
+        }
+    }
+   return [count,pair]
+   
 };
+
+let nums = [1,3,2,1,3,2,2]; 
+let x  = numberOfPairs(nums)
+console.log(x);
