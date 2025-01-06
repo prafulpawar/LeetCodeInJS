e597bc7678865984c193101ccf08e91ad6425cfa
prafulpawar <prafuldukhi@gmail.com>
2025-01-06 14:06:20 +0530

    "Done"

--- Basics/Arrys/118.js
+++ Basics/Arrys/118.js
@@ -4,7 +4,7 @@
  * @return {number[][]}
  */
 
-function fact (){
+function fact (numRows,cols){
     
   function calculateFactorial(n) {
     if (n === 0 || n === 1) {
@@ -19,21 +19,26 @@ function fact (){
 
   
   let factNumRows =  calculateFactorial(numRows);
-  let factCol = calculateFactorial(col);
-  
+  let factCol = calculateFactorial(cols);
+  let factRowCol = calculateFactorial(numRows - cols);
+  let ans = factNumRows / (factCol - factRowCol);
+
+  console.log(factNumRows,factCol,factRowCol)
+  console.log(ans)
   
 }
 var generate = function (numRows) {
     const arr = [];
-     numRows = 3;
+    
 
     // Dynamically filling rows and columns
     for (let i = 0; i < numRows; i++) {
         const cols = i + 1; // Number of columns increases with each row
         arr[i] = [];
         for (let j = 0; j < cols; j++) {
-               fact=(numRows,col)
-            arr[i][j] = (i + 1) * (j + 1);  // Filling with some values
+              let a = fact(numRows,j)
+            //   console.log(a)
+            // arr[i][j] = (i + 1) * (j + 1);  // Filling with some values
         }
     }
     return arr
