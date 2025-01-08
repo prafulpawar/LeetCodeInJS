59a56f290a3b463f057fe2a546734da9d713c9b1
prafulpawar <prafuldukhi@gmail.com>
2025-01-08 18:38:42 +0530

    "Done"

---
+++ Basics/Arrys/2465.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var distinctAverages = function(nums) {
+    // let i = nums.length
+    // while(nums[i]!= 0)
+};
+
+let nums = [1,100]
+let x = distinctAverages(nums)
+console.log(x);
