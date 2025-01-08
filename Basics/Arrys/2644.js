c6c47cb7caba25e5f10fc75be4a3aebc18a03d25
prafulpawar <prafuldukhi@gmail.com>
2025-01-09 02:21:15 +0530

    "Done"

--- Basics/Arrys/2644.js
+++ Basics/Arrys/2644.js
@@ -3,6 +3,13 @@
  * @param {number[]} divisors
  * @return {number}
  */
-var maxDivScore = function(nums, divisors) {
-    
+var maxDivScore = function(nums, divisors) {   
+      for(let i =0;i<nums.length;i++){
+
+      }
 };
+
+let nums = [2,9,15,50]
+let divisors = [5,3,7,2]
+let x = maxDivScore(nums, divisors)
+console.log(x)
