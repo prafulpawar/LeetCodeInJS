11e6150e90e2c0f2acb7f67b355639f7be3d30bb
prafulpawar <prafuldukhi@gmail.com>
2025-01-08 19:29:34 +0530

    "Done"

--- Basics/Arrys/2465.js
+++ Basics/Arrys/2465.js
@@ -2,9 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+function countDistinctNumbers(arr) {
+    const distinct = arr.filter((value, index, self) => self.indexOf(value) === index);
+    return distinct.length;
+}
+
 var distinctAverages = function(nums) {
-    // let i = nums.length
-    // while(nums[i]!= 0)
+    nums = nums.sort((a, b) => a - b)
+    console.log(nums)
+    let size = nums.length-1
+    let avg = []
+    for(let i=0;i<nums.length;i++){
+        let sum = (nums[i]+nums[size] )
+        avg.push(sum/2)
+        size--
+    }
+    
+   let x = countDistinctNumbers(avg)
+   return x
 };
 
 let nums = [1,100]
---
+++ Basics/Arrys/2591.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} money
+ * @param {number} children
+ * @return {number}
+ */
+var distMoney = function(money, children) {
+    
+};
+
+let money = 20
+let children = 3
+
+console.log(distMoney(money, children)) 
