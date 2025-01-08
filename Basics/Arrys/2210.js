5af391266a666a81dac8f1a6ebda6439988b27da
prafulpawar <prafuldukhi@gmail.com>
2025-01-08 14:28:00 +0530

    "Done"

--- Basics/Arrys/2210.js
+++ Basics/Arrys/2210.js
@@ -2,18 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+
 var countHillValley = function(nums) {
-    let vally = 0;
-    let hill = 0;
-     for(let i =0 ; i< nums.length;i++){
-        if(nums[i] > nums[i+1] && nums[i] > nums[i-1]  ){
-             vally++;
+   
+     let count = 0; 
+    let prev = nums[0]
+     for(let i =1 ; i< nums.length;i++){
+           
+        if (nums[i] === nums[i + 1]) continue;
+
+       
+        if ((nums[i] > prev && nums[i] > nums[i + 1]) ||  
+            (nums[i] < prev && nums[i] < nums[i + 1])) {  
+            count++; 
+            prev = nums[i]; 
         }
-        if (nums[i] < nums[i-1] && nums[i] < nums[i-1]   ){
-            hill++;
-        }
-     }
-     console.log(vally, ' ',hill)
+    }
+
+    return count;
 };
 let nums = [2,4,1,1,6,5]
 let x  = countHillValley(nums);
