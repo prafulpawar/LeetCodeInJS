3eb13ec5b893433580240b93651be7436b2a2639
prafulpawar <prafuldukhi@gmail.com>
2025-01-08 14:46:17 +0530

    "Done"

--- Basics/Arrys/1732.js
+++ Basics/Arrys/1732.js
@@ -5,9 +5,10 @@ var largestAltitude = function(gain) {
      let currentAltitude = 0;
      
      for(let i=0;i<gain.length;i++){
-         
+          currentAltitude += gain[i];
+          maxAltitude = Math.max(maxAltitude,currentAltitude);
      }
- 
+ return maxAltitude;
 };
 let gain =[-5,1,5,0,-7]
 
