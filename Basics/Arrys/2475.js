e07716b7e8a79ecd24d97cb084b39a70b8cd6afc
prafulpawar <prafuldukhi@gmail.com>
2025-01-09 00:24:18 +0530

    "Done"

--- Basics/Arrys/2475.js
+++ Basics/Arrys/2475.js
@@ -6,11 +6,13 @@ var unequalTriplets = function(nums) {
     nums.sort((a,b) => a - b)  // sorting the array in ascending order
     let count =0
     for(let i =0 ; i < nums.length-1 ; i++) {
-        for(let j = i+1 ; j < nums.length ; j++) {
-            if(nums[i]!=nums[j] && nums[i]!=nums[j+1] && nums[j]!=nums[j+1]) {
-                count++
+        for(let j = i+1  ; j < nums.length ; j++) {
+            for(let k = j+1 ; k < nums.length ;k++){
+                if(nums[i]!==nums[j] && nums[j]!==nums[k] && nums[i]!==nums[k]){
+                    count++
+                }
             }
-        } 
+        }
     }
     return count;
 };
