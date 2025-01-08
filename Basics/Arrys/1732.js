cf636bcc746dbc7a1150a8c36c953914baecbe68
prafulpawar <prafuldukhi@gmail.com>
2025-01-08 14:41:19 +0530

    "Gain"

---
+++ Basics/Arrys/1732.js
@@ -0,0 +1,15 @@
+
+
+var largestAltitude = function(gain) {
+     let maxAltitude = 0;
+     let currentAltitude = 0;
+     
+     for(let i=0;i<gain.length;i++){
+         
+     }
+ 
+};
+let gain =[-5,1,5,0,-7]
+
+let x = largestAltitude(gain);
+console.log(x)
