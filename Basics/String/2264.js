0fa50ebf9d76a665e519ac8b609c54c67b7bd7f9
prafulpawar <prafuldukhi@gmail.com>
2025-01-10 21:13:10 +0530

    "Done"

---
+++ Basics/String/2264.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestGoodInteger = function(num) {
+    let count=0;
+    let newArr=[]
+      for(let i=0;i<num.length;){
+         if(num[i] == num[i+1] && num[i+2] ==num[i]  ){
+           
+            count=3;
+            
+         }
+         if(count === 3 ){
+            
+            newArr.push(num[i]);
+            count=0;
+            i=i+2
+         }
+         else{
+            i++
+         }
+         
+      }
+      if(newArr.length <1){
+         return "";
+      }
+      else{
+          let max = newArr[0];
+          for(let i=0;i<newArr.length;i++){
+             if(Number(newArr[i]) > Number(max)){
+                max = newArr[i];
+             }
+          }
+          
+          return  Array(3).fill(max).join("");
+      }
+
+};
+
+let num = "014455"
+
+let x  = largestGoodInteger(num);
+console.log(x)
--- Basics/String/2287.js
+++ Basics/String/2287.js
@@ -1,26 +1,57 @@
-function calculateCharFrequency(str) {
-    const frequency = {};
+function rearrangeCharacters(str, target) {
+  // const frequency = new Array(26).fill(0);
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str[i] >= 'a' && str[i] <= 'z') {
+  //     const index = str[i].charCodeAt(0) - 97;
+  //     frequency[index]++;
+  //   }
+  // }
+ 
+  //  
   
-    for (const char of str) {
-      if (frequency[char]) {
-        frequency[char]++;
-      } else {
-        frequency[char] = 1;
-      }
-    }
-  
-    return frequency;
+  // for (let i = 0; i < frequency.length; i++) {
+  //   if (frequency[i] > 0) {
+       
+  //          console.log(`Character: ${String.fromCharCode(i + 97)}, Count: ${frequency[i]}`);
+  //      newArr.push(String.fromCharCode(i + 97))
+      
+  //   }
+  // }
+  function removeDuplicates(arr) {
+    return [...new Set(arr)];
   }
-  
-  
+  function findMin(arr) {
+    if (arr.length === 0) {
+        return null; // Handle empty array
+    }
+    return Math.min(...arr); // Spread operator to pass array elements
+}
+   target = removeDuplicates(target)
+  let newArr=[]
+  for(let i=0;i<target.length;i++){
+    let count=0;
+      for(let j=0;j<str.length;j++){
+          if(target[i] === str[j] ){
+            count++;
+          }
+      }
+      newArr.push(count)
+      
+  } 
 
-var rearrangeCharacters = function(s, target) {
-    let x = calculateCharFrequency(s)
-    console.log(x);
-    
-};
+  if(newArr.length === 1){
+     return 1
+  }
+  else{
+    return findMin(newArr)  
+  }
+ 
 
-let s = "ilovecodingonleetcode"
-let target ="code"
-let x = rearrangeCharacters(s,target)
+
+
+}
+
+let s ="abbaccaddaeea"
+let target ="aaaaa"
+let x = rearrangeCharacters(s, target);
 console.log(x)
