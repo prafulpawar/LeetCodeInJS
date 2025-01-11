9b1032836b5e4515efdca8a4c71226dc3d30c831
prafulpawar <prafuldukhi@gmail.com>
2025-01-11 11:28:00 +0530

    "Done"

--- Basics/String/2283.js
+++ Basics/String/2283.js
@@ -2,10 +2,24 @@
  * @param {string} num
  * @return {boolean}
  */
+
+
 var digitCount = function(num) {
-    
+   for(let i=0;i<num.length;i++){
+       let count = 0;
+        for(let j=0;j<num.length;j++){
+             if(i == Number(num[j])){
+                count++;
+             }
+        }
+        if(num[i] !== String(count)){
+            return false;
+        }
+       
+   }
+   return true;
 };
 
-let num = "1210"
+let num = '030'
 let x = digitCount(num)
 console.log(x);
