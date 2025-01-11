7ec137501508da0b071b0833c98e0aaa62d6907a
prafulpawar <prafuldukhi@gmail.com>
2025-01-11 22:16:53 +0530

    "Done"

--- Basics/String/2273.js
+++ Basics/String/2273.js
@@ -2,13 +2,26 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var removeAnagrams = function(words) {]
+var removeAnagrams = function(words) {
+    let testArr=words
+    
+    let newArr=[];
      for(let i=0;i<words.length;i++){
-         let j= words[i];
-         console.log(j);
+         let j= words[i].split('').sort().join('');
+         newArr.push(j);
      }
+     
+   let k=0
+   words=[]
+    for(let i=0;i<newArr.length;i++){
+        if(newArr[i] !== newArr[i+1]){
+            words.push(testArr[k]);
+            k=i+1;
+        }
+        
+    }
+    return words;
 };
 
 let words = ["abba","baba","bbaa","cd","cd"]
-
 console.log(removeAnagrams(words)) 
