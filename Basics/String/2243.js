cedf5aefc9efd12ac97d8dcbe940d39a3979ecc9
prafulpawar <prafuldukhi@gmail.com>
2025-01-12 16:15:07 +0530

    "Done"

--- Basics/String/2243.js
+++ Basics/String/2243.js
@@ -4,12 +4,30 @@
  * @return {string}
  */
 var digitSum = function(s, k) {
-     let arr=[];
-     for(let i=0;i<s.length;){
+    while (s.length > k) {
+        let arr = [];
         
-     }
+        // Iterate through the string in chunks of size `k`
+        for (let i = 0; i < s.length; i += k) {
+            let sum = 0;
+
+            // Sum up the digits in the current chunk
+            for (let j = i; j < Math.min(i + k, s.length); j++) {
+                sum += Number(s[j]);
+            }
+
+            arr.push(sum); // Add the sum of the current chunk to the array
+        }
+
+        // Update `s` with the new summed array as a string
+        s = arr.join('');
+    }
+
+    return s;
+    
+       
 };
 
-let s = "11111222223", k = 3
+let s ="11111222223", k = 3
 
-console.log(digitSum(s, k)) // Output: "1123"
+console.log(digitSum(s, k)) 
