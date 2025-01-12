b186e76f9f5654f3c329a4a60266f200a59ccc9e
prafulpawar <prafuldukhi@gmail.com>
2025-01-12 22:38:25 +0530

    "Done"

--- Basics/String/2325.js
+++ Basics/String/2325.js
@@ -3,16 +3,15 @@
  * @param {string} message
  * @return {string}
  */
-var decodeMessage = function(key, message) {
 
-    console.log(key,'\n', message);
+
+
+
+var decodeMessage = function(key, message) {
 
 };
 
 
 let key   = "the quick brown fox jumps over the lazy dog"
 let message = "vkbs bs t suepuv"
-
 let x = decodeMessage(key,message);
-
-// console.log(x)
