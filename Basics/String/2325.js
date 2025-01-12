2f6e0fb6507d2766b5edff8d9a2ab063168fddd6
prafulpawar <prafuldukhi@gmail.com>
2025-01-12 23:35:50 +0530

    "Done"

--- Basics/String/2325.js
+++ Basics/String/2325.js
@@ -8,7 +8,34 @@
 
 
 var decodeMessage = function(key, message) {
-
+    let arr =[];
+      let count=0;
+      for(let i=0;i<key.length;i++){
+          if(key[i]!==' '){
+           arr.push(key[i])
+            count++;
+          }
+      }
+    let arr2=[];
+    for(let i=0;i<message.length;i++){
+        if(message[i]!==' '){
+         arr2.push(message[i])
+          count++;
+        }
+    }
+      
+      const uniqueArr = [...new Set(arr)];
+    
+     console.log(uniqueArr)
+     console.log(arr2)
+     
+     for(let i=0;i<arr2.length;i++){
+         for(let j=0;j<uniqueArr.length;j++){
+             if(arr2[i]===uniqueArr[j]){
+                console.log(arr2[i] ,' ', String.fromCharCode(i+97),' ', uniqueArr[i]  )
+             }
+         }
+     }
 };
 
 
