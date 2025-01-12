7b0dcf8ceb2b9485084f2af6209395a44fdfa6d3
prafulpawar <prafuldukhi@gmail.com>
2025-01-12 22:25:40 +0530

    "Done"

---
+++ Basics/String/2325.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} key
+ * @param {string} message
+ * @return {string}
+ */
+var decodeMessage = function(key, message) {
+
+    console.log(key,'\n', message);
+
+};
+
+
+let key   = "the quick brown fox jumps over the lazy dog"
+let message = "vkbs bs t suepuv"
+
+let x = decodeMessage(key,message);
+
+// console.log(x)
--- Basics/String/2404.js
+++ Basics/String/2404.js
@@ -2,25 +2,36 @@
  * @param {number[]} nums
  * @return {number}
  */
-var mostFrequentEven = function(nums) {
-    let newArr = [];
-      for(let i=0;i<nums.length;i++){
-  
-         if( Number(nums[i]) % 2 === 0 && Number(nums[i])>0 ){
-                 newArr.push(nums[i]);
-         }
-      }
-      
-     let min = Math.min(...newArr)
 
-     if(min ===Infinity){
+
+function countFrequency(arr) {
+    const frequency = {};
+    for (let item of arr) {
+        frequency[item] = (frequency[item] || 0) + 1;
+    }
+    return frequency;
+}
+var mostFrequentEven = function (nums) {
+    let newArr = [];
+    for (let i = 0; i < nums.length; i++) {
+
+        if (Number(nums[i]) % 2 === 0) {
+            newArr.push(nums[i]);
+        }
+    }
+    if (newArr.length === 0) {
         return -1
-     }
-     else{
-        return min
-     }
+    }
+
+    let obj =  countFrequency(newArr)
+    console.log(obj) 
+   
+   
+    
+
+
 };
 
-let nums = [4,4,4,9,2,4]
-let x    = mostFrequentEven(nums);
+let nums =[2,10000,10000,10000,2]
+let x = mostFrequentEven(nums);
 console.log(x);
