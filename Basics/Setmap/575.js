b418b57b31111681382184af0b7911820c404a19
prafulpawar <prafuldukhi@gmail.com>
2025-01-13 19:25:30 +0530

    "Done"

---
+++ Basics/Setmap/575.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+    let set = new Set(candyType)
+    if(set.size===1){
+        return 1
+    }
+    else{
+        let min=0;
+        let mincan=0
+        min=candyType.length/2;
+        mincan=set.size;
+
+        if(min>mincan){
+            return mincan
+        }
+        else{
+            return min
+        }
+    }
+};
+
+let candyType =[1,1,1,1,2,2,2,3,3,3]
+
+let x = distributeCandies(candyType);
+console.log(x);
