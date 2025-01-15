4517d4f601bd7fb1676d9d114ecb68c7052206fe
prafulpawar <prafuldukhi@gmail.com>
2025-01-15 22:17:18 +0530

    "Done"

---
+++ Basics/String/2053.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function(arr, k) {
+    
+};
+
+let arr = ["d","b","c","b","c","a"], k = 2
+let x = kthDistinct(arr,k);
+console.log(x);
