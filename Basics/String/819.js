6be3d7141a1645ab1d2be04c6daabf0e705f8bfb
prafulpawar <prafuldukhi@gmail.com>
2025-01-16 00:25:18 +0530

    "Done"

---
+++ Basics/String/819.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+    
+};
+
+let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+
+let x = mostCommonWord(paragraph, banned);
+console.log(x);
