64eb93649c7fa6cb1be086d62bfe1c49494d4b66
prafulpawar <prafuldukhi@gmail.com>
2025-01-15 11:50:14 +0530

    "Done"

--- Basics/Setmap/freqarr.js
+++ Basics/Setmap/freqarr.js
@@ -1,13 +1,19 @@
-let arr = [2,1,2,3,1,1,2,2,3,5,6,7,6]
+let arr = [2,2,2,2,2,2,2]
 let map = new Map()
 
 for(let i=0;i<arr.length;i++){
-     if(map.has(arr[i])){
-        map.set(arr[i], map.get(arr[i])+1) 
+    //  if(map.has(arr[i])){
+    //     map.set(arr[i], map.get(arr[i])+1) 
 
-     }
-     else{
-         map.set(arr[i], 1)
-     }
+    //  }
+    //  else{
+    //      map.set(arr[i], 1)
+    //  }
+
+    map.set(arr[i], (map.get(arr[i]) || 0) + 1)  //single line
+    
 }
 console.log(map)
+
+// 
+// 
---
+++ Basics/Setmap/uni.js
@@ -0,0 +1,22 @@
+let arr = [2,4,2,1,5,4,6,7,6,4,6]
+let map = new Map()
+
+for(let i=0;i<arr.length;i++){
+    map.set(arr[i], (map.get(arr[i]) || 0) + 1)  //single line
+}
+// for(let i=0;i<arr.length;i++){
+//       if(map.get(arr[i]) === 1){
+//          console.log(arr[i])  
+//     }
+   
+// }
+//Another Way
+for(let a of map.keys()){
+    if(map.get(a)===1){
+        console.log(a)
+    }
+ 
+}
+
+
+// console.log(map)
