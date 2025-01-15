cfb92895a37230391be6b813e3b2c4772c17d92d
prafulpawar <prafuldukhi@gmail.com>
2025-01-15 22:35:54 +0530

    "Done"

--- Basics/String/2053.js
+++ Basics/String/2053.js
@@ -5,13 +5,24 @@
  */
 var kthDistinct = function(arr, k) {
     let map = new Map()
-
+    let arr2 = [];
     for(let i=0;i<arr.length;i++){
         map.set(arr[i], (map.get(arr[i]) || 0) + 1)  
     }
-    console.log(map)
+   
+    for (let [key, value] of map) {
+         if(value===1){
+            arr2.push(key)
+         }
+    }
+    for(let i=0;i<arr2.length;i++){
+        if(i===k-1){
+            return arr2[i]
+        }
+    }
+    return "";
 };
 
-let arr = ["d","b","c","b","c","a"], k = 2
+let arr =["d","b","c","b","c","a"], k = 2
 let x = kthDistinct(arr,k);
 console.log(x);
