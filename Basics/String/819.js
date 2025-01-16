cc18ae80a07331d42ba8c38dd1653bb62fd754f1
prafulpawar <prafuldukhi@gmail.com>
2025-01-16 11:40:51 +0530

    "Done"

--- Basics/String/819.js
+++ Basics/String/819.js
@@ -4,7 +4,28 @@
  * @return {string}
  */
 var mostCommonWord = function(paragraph, banned) {
+    paragraph = paragraph.toLowerCase().replace(/[^a-z\s]/g, '');
+     let map = new Map();
+    const result = paragraph.split(' ')
+
+    for(let i = 0; i < result.length; i++) {
+        map.set(result[i], (map.get(result[i]) || 0) + 1)
+    }
+    // console.log(map);
     
+
+    let max ='';
+    let k=0;
+    for(let [key,value] of map){
+        if(!map.has(banned[k])){
+            
+            continue;
+        }
+        else{
+            console.log(key,' ', value,' ',banned[k])
+        }
+        // k++;
+    }
 };
 
 let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
