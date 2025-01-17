4b553b8c5e9d3a26634d89b9a5406047caa5cda3
prafulpawar <prafuldukhi@gmail.com>
2025-01-17 23:26:21 +0530

    "Done"

--- Basics/String/125.js
+++ Basics/String/125.js
@@ -2,11 +2,34 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    s=s.toLowerCase();
-    console.log(s)
+var isPalindrome = function (s) {
+    if(s.length===1){
+        return true;
+    }
+    s = s.toLowerCase();
+    
+     
+    const arr = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122 || char.charCodeAt(0)>=48 && char.charCodeAt(0)<=57   ) {
+            arr.push(char);
+        }
+    }
+  
+   
+    let size = arr.length-1
+    for(let i=0; i<arr.length;i++){
+        if(arr[i] !== arr[size]){
+            return false;
+        }
+        size--;
+    }
+     return true;
+    
 };
 
-let  s = "A man, a plan, a canal: Panama"
-let  x = isPalindrome(s);
+let s ="a."
+let x = isPalindrome(s);
 console.log(x);
