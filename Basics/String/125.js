322d78efda71a7cd2fbf468777e65df576012bdd
prafulpawar <prafuldukhi@gmail.com>
2025-01-17 22:53:45 +0530

    "Done"

---
+++ Basics/String/125.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s=s.toLowerCase();
+    console.log(s)
+};
+
+let  s = "A man, a plan, a canal: Panama"
+let  x = isPalindrome(s);
+console.log(x);
--- Basics/String/2404.js
+++ Basics/String/2404.js
@@ -7,6 +7,7 @@
 
 
 var mostFrequentEven = function (nums) {
+  let map = new Map();
     let newArr = [];
     for (let i = 0; i < nums.length; i++) {
 
@@ -17,9 +18,10 @@ var mostFrequentEven = function (nums) {
     if (newArr.length === 0) {
         return -1
     }
-
-    
-
+    for(let i=0; i<newArr.length; i++){
+        map.set(newArr[i], (map.get(newArr[i]) || 0) + 1);
+    }
+    console.log(map)
 };
 
 let nums =[0,1,2,0,0,0,2,4,4,1]
