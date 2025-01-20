02c67b46655ad1616410538043bc117f1a192ed9
prafulpawar <prafuldukhi@gmail.com>
2025-01-20 11:39:56 +0530

    "Recursion"

---
+++ Recursion/base.js
@@ -0,0 +1,14 @@
+function main(){
+    greet()
+    console.log("Main chala....")
+}
+function greet(){
+    enjoy()
+     console.log("Good Morning pipeline....")
+}
+
+function enjoy(){
+      console.log("Enjoy Ho Raha Hai Guys....")
+}
+
+main()
