6b72065bee79198f62577c36b5692508fa6f7242
prafulpawar <prafuldukhi@gmail.com>
2025-01-20 11:29:04 +0530

    "Done"

---
+++ Basics/String/859.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function(s, goal) {
+    
+};
+
+
+let s = "ab", goal = "ba"
+let x = buddyStrings(s,goal)
+console.log(x);
