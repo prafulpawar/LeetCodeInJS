c537c062f7dbb9342c280f0e57461d96a7b39637
prafulpawar <prafuldukhi@gmail.com>
2025-01-22 11:04:16 +0530

    'Added_String'

--- Basics/String/1528.js
+++ Basics/String/1528.js
@@ -4,5 +4,19 @@
  * @return {string}
  */
 var restoreString = function(s, indices) {
-    
+     let arr=[];
+     s = Array.from(s)
+     console.log(s)
+     console.log(s)
+    for(let i=0;i<indices.length;i++){
+        for(let j=0;j<s.length;j++){
+             if(indices[i]===j){
+                arr.push(s[j])
+             }
+        }
+    }
+    return arr.join("")
 };
+s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+let x  = restoreString(s,indices)
+console.log(x)
