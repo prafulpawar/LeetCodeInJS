78a9d956e24c74cf74342b9e1e5193c2ed36d1d2
prafulpawar <prafuldukhi@gmail.com>
2025-01-22 11:34:37 +0530

    'AddedGCDQuestion'

---
+++ Recursion/gcd.js
@@ -0,0 +1,16 @@
+function gcd(a,b,c){
+    if(c===1){
+        return 1
+    }
+    if(a%c===0 && b%c===0){
+     return c
+    }
+
+   return gcd(a,b,c-1);
+
+}
+
+let a=210,b=3270;
+let ans = Math.min(a,b);
+let x = gcd(a,b,ans)
+console.log(x)
