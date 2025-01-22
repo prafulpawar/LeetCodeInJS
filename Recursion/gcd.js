8602826a7ceb8a28fca157feef55044dc0aefc46
prafulpawar <prafuldukhi@gmail.com>
2025-01-22 11:52:35 +0530

    'Added One More'

--- Recursion/gcd.js
+++ Recursion/gcd.js
@@ -14,3 +14,18 @@ let a=10,b=20;
 let ans = Math.min(a,b);
 let x = gcd(a,b,ans)
 console.log(x)
+
+
+let n = 5
+temp(n)
+
+function temp(n){
+    if(n===0){
+        return
+    }
+    console.log('Hello World');
+    temp(n=n-1)
+}
+
+// n-- pahle aap use fir change value hongi
+// n-1 
