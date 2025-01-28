773de944ac4f60b285235834e8d6e1acac59f176
prafulpawar <prafuldukhi@gmail.com>
2025-01-28 12:23:53 +0530

    'DoneDuplicate'

--- Recursion/50.js
+++ Recursion/50.js
@@ -4,9 +4,35 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-     
+      if(n===0){
+        return 1.0;
+      }
+      if(n<0) {
+        ans= 1/solve(x,n)
+        return ans
+      }
+      else{
+          ans = solve(x,n)
+          return ans
+      }
 };
+function solve (x,n){
+    if(n===0){
+         return 1
+    }
+
+   s= solve(x,parseInt(n/2));
+   if(n%2===0){
+      return s*s
+   }
+   else{
+    return s*s*x
+   }
+
+}
+
+
 let x=2.0000
-let n =-2
+let n =10
 let p = myPow(x,n)
 console.log(p)
