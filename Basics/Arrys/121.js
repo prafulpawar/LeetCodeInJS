373c944488be01663755d930995d161f153f01d3
prafulpawar <prafuldukhi@gmail.com>
2025-01-29 14:31:51 +0530

    '121' done

--- Basics/Arrys/121.js
+++ Basics/Arrys/121.js
@@ -7,28 +7,7 @@ for (let i = 0; i < size; i++) {
 }
 
 var maxProfit = function(prices) {
-    let min = Infinity; 
-    let inmin = -1;     
-    
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] > 0 && prices[i] < min) { 
-            min = prices[i]; 
-            inmin = i;    
-        }
-    }
-         let ans =0
-         let copy = 0
-    
-         for(let i =inmin ; i<prices.length;i++){
-                if(ans >copy){
-                      copy =ans
-                }
-                ans = prices[i]-min
-         }
-         if(ans >copy){
-            copy =ans
-      }
-         return copy
+   
 }
 
 let x = maxProfit(prices)
