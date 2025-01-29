dca613cadc7a8d2d9d4769dc57a26deed8295cf1
prafulpawar <prafuldukhi@gmail.com>
2025-01-29 14:49:26 +0530

    'Done121'

--- Basics/Arrys/121.js
+++ Basics/Arrys/121.js
@@ -7,7 +7,14 @@ for (let i = 0; i < size; i++) {
 }
 
 var maxProfit = function(prices) {
-   
+    let maxProfit = 0;
+    let minPrice = Infinity; 
+
+    for (let i = 0; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]); // Har step par minimum price update karna hai
+        let profit = prices[i] - minPrice; // Current price par bechne par jo profit hoga
+        maxProfit = Math.max(maxProfit, profit); // Maximum profit update karna hai
+    }
 }
 
 let x = maxProfit(prices)
