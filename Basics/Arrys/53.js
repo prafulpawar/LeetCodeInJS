fa6b3a308611fbde87522b30636ff8ee0301aa48
prafulpawar <prafuldukhi@gmail.com>
2025-01-30 23:08:41 +0530

    'Basics'

--- Basics/Arrys/53.js
+++ Basics/Arrys/53.js
@@ -2,16 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+// this is bruthForce Approch
+// var maxSubArray = function(nums) {
 
-    let maxSum     = -Infinity;
-    let currentSum = 0;
+//     let maxSum     = -Infinity;
+//     let sum =  -Infinity;
+//     let currentSum = 0;
+    
+//     for(let i=0;i<nums.length;i++){
+//         currentSum=0;
+//         for(let j=i; j<nums.length;j++){
+//             currentSum=nums[j]+currentSum
+//             maxSum = Math.max(maxSum,currentSum)
+//             console.log(`Subarray (${i} to ${j}): Sum = ${currentSum}, Max so far = ${}`);
+//         }
+//     }
+//     return maxSum
 
-    for(let i=0;i<nums.length;i++){
-    }
+// };
 
-};
+// let nums =[-2,1,-3,4,-1,2,1,-5,4]
+// let x = maxSubArray(nums)
+// console.log(x)
 
-let nums = [-2,1,-3,4,-1,2,1,-5,4]
-let x = maxSubArray(nums)
-console.log(x)
+// Kadane's Algorithm
