257b7309858fbcd3d81538bfb589f4ee6ccb0a2c
prafulpawar <prafuldukhi@gmail.com>
2025-01-31 00:01:16 +0530

    'Done'

--- Basics/Arrys/53.js
+++ Basics/Arrys/53.js
@@ -5,24 +5,44 @@
 // this is bruthForce Approch
 // var maxSubArray = function(nums) {
 
-//     let maxSum     = -Infinity;
-//     let sum =  -Infinity;
+//     let maxSum     = 0;
 //     let currentSum = 0;
     
 //     for(let i=0;i<nums.length;i++){
 //         currentSum=0;
 //         for(let j=i; j<nums.length;j++){
 //             currentSum=nums[j]+currentSum
+//              if(currentSum<0){
+//                 sum=0
+//                 break;
+//              }
 //             maxSum = Math.max(maxSum,currentSum)
-//             console.log(`Subarray (${i} to ${j}): Sum = ${currentSum}, Max so far = ${}`);
+//             console.log(`Subarray (${i} to ${j}): Sum = ${currentSum}, Max so far = ${maxSum}`);
 //         }
 //     }
 //     return maxSum
 
 // };
 
-// let nums =[-2,1,-3,4,-1,2,1,-5,4]
+// let nums =[2, 3, -8, 7, -1, 2, 3]
 // let x = maxSubArray(nums)
 // console.log(x)
 
 // Kadane's Algorithm
+
+var maxSubArray = function(nums) {
+
+    let maxSum     = 0;
+    let currentSum = 0;
+    
+    for(let i=0;i<nums.length;i++){
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    return maxSum
+
+};
+
+let nums =[2, 3, -8, 7, -1, 2, 3]
+let x = maxSubArray(nums)
+console.log(x)
