f0a71a86d82267816200eea290a34b742f5d914b
prafulpawar <prafuldukhi@gmail.com>
2025-01-30 21:47:31 +0530

    'Done'

--- Basics/Arrys/53.js
+++ Basics/Arrys/53.js
@@ -3,7 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+
+    let maxSum     = -Infinity;
+    let currentSum = 0;
+
+    for(let i=0;i<nums.length;i++){
+    }
+
 };
 
 let nums = [-2,1,-3,4,-1,2,1,-5,4]
