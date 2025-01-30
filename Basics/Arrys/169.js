f9b667f8c630298e821fde95030642a58e1663c9
prafulpawar <prafuldukhi@gmail.com>
2025-01-31 00:46:56 +0530

    '169_Done'

--- Basics/Arrys/169.js
+++ Basics/Arrys/169.js
@@ -3,10 +3,21 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    
+    majority = Math.floor(nums.length/2);
+    for(let i=0;i<nums.length;i++){
+        let count=0;
+        for(let j=0;j<nums.length;j++){
+             if(nums[i] === nums[j]){
+                count++;
+             }
+        }
+        if(count>majority){
+            return nums[i]
+        }
+    }
 };
 
 
-let nums = [2,2,1,1,1,2,2];
+let nums = [6,5,5];
 let ans  = majorityElement(nums)
-1
+console.log(ans)
