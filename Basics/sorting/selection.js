7c551195f3a810229997f68ad37af7dea3129669
prafulpawar <prafuldukhi@gmail.com>
2025-02-07 12:06:34 +0530

    selection_Sort_Done

--- Basics/sorting/selection.js
+++ Basics/sorting/selection.js
@@ -1,6 +1,17 @@
-let arr = [7,4,3,5,1,9]
+let arr = [7, 4, -13, 5, 1, 9];
 
-for(let i=0;i<arr.length;i++){
-    let minIndex = 0;
-    
+for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+            minIndex = j; 
+        }
+    }
+    if (minIndex !== i) {
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
 }
+
+console.log(arr);
