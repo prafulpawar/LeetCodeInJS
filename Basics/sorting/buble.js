46a9c127ca336af46287132315448f45870e766c
prafulpawar <prafuldukhi@gmail.com>
2025-02-07 12:17:17 +0530

    bubble_Sort

--- Basics/sorting/buble.js
+++ Basics/sorting/buble.js
@@ -1,13 +1,13 @@
-let arr = [7,8,5,6,7,1,2,7,6,2,3,7,1,7,3,3,2,5,1,2,3]
+let arr = [7,8,5,6,7,1,2,-7,6,2,3,7,1,7,3,3,2,5,1,2,3];
 
-for(let i=0;i<arr.length-1;i++){
-
-    for(let j=i+1;j<arr.length;j++){
-           if(arr[i]>arr[j]){
-               let temp = arr[i];
-               arr[i] = arr[j];
-               arr[j] = temp
-           }
+for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
     }
 }
-console.log(arr)
+
+console.log(arr);
