113d512482afd8fae3a8020a31c6690dcc5eadff
prafulpawar <prafuldukhi@gmail.com>
2025-02-10 23:28:50 +0530

    ___Done__Done

--- Basics/String/859.js
+++ Basics/String/859.js
@@ -4,13 +4,29 @@
  * @return {boolean}
  */
 var buddyStrings = function(s, goal) {
-   
-    let arr = Array.from(s);
-    let goalarr = Array.from(goal)
-    console.log(arr,goalarr)
+    s=new Set(s)
+    goal = new Set(goal)
+  
+     s= Array.from(s);
+     goal = Array.from(goal)
+     let j=0;
+     let i=0;
+    for(i;i<s.length;i++){
+        if(s[i] === goal[j] ){
+            j++;
+        }
+    }
+    console.log(i,' ',j)
+
+    // if(s === goal){
+    //     return true
+    // }
+    // else{
+    //     return false
+    // }
 };
 
 
-let s = "aa", goal = "aa"
+let s = "aaaaaaa", goal ="aaaaaaa"
 let x = buddyStrings(s,goal)
 console.log(x);
