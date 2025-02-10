3d2a9bb0d040024e0ff8eeeb318226a2da63dd09
prafulpawar <prafuldukhi@gmail.com>
2025-02-10 11:48:25 +0530

    __Merge_Done

--- Basics/sorting/merge.js
+++ Basics/sorting/merge.js
@@ -4,6 +4,27 @@ divide(arr,0,arr.length-1);
 
 function divide (arr,first,last){
     if(first<last){
-        
+
+        let mid = first + (last-first)/2;
+        divide(arr,first,mid)
+        divide(arr,mid+1,last)
+        conquer(arr,first,)
     }
+}
+function conquer (arr,first,mid,last){
+      let temp = new Array(last-first-1);
+      let idx1 = first
+      let idx2 = mid+1
+      let k=0
+      while(idx1<=mid && idx2<=last){
+        if(arr[idx1] < arr[idx2]){
+            temp[k] = arr[idx1]
+            idx1++;
+        }
+        else{
+            temp[k] = arr[idx2]
+            idx1++;
+        }
+        k++
+      }
 }
