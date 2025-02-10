16909b3c466b385d8336ec94ffeb1b38aedee774
prafulpawar <prafuldukhi@gmail.com>
2025-02-10 11:59:22 +0530

    __Merge_Sort_Done

--- Basics/sorting/merge.js
+++ Basics/sorting/merge.js
@@ -1,30 +1,39 @@
-let arr = [7,83,2,1,3,5,2,9,6];
+let arr = [7, 83, 2, 1, 3, 5, 2, 9, 6];
 
-divide(arr,0,arr.length-1);
+divide(arr, 0, arr.length - 1);
 
-function divide (arr,first,last){
-    if(first<last){
+console.log(arr);
 
-        let mid = first + (last-first)/2;
-        divide(arr,first,mid)
-        divide(arr,mid+1,last)
-        conquer(arr,first,)
+function divide(arr, first, last) {
+    if (first < last) {
+        let mid = Math.floor(first + (last - first) / 2);
+        divide(arr, first, mid);
+        divide(arr, mid + 1, last);
+        conquer(arr, first, mid, last);
     }
 }
-function conquer (arr,first,mid,last){
-      let temp = new Array(last-first-1);
-      let idx1 = first
-      let idx2 = mid+1
-      let k=0
-      while(idx1<=mid && idx2<=last){
-        if(arr[idx1] < arr[idx2]){
-            temp[k] = arr[idx1]
-            idx1++;
+
+function conquer(arr, first, mid, last) {
+    let temp = new Array(last - first + 1);
+    let idx1 = first, idx2 = mid + 1, k = 0;
+
+    while (idx1 <= mid && idx2 <= last) {
+        if (arr[idx1] < arr[idx2]) {
+            temp[k++] = arr[idx1++];
+        } else {
+            temp[k++] = arr[idx2++];
         }
-        else{
-            temp[k] = arr[idx2]
-            idx1++;
-        }
-        k++
-      }
+    }
+
+    while (idx1 <= mid) {
+        temp[k++] = arr[idx1++];
+    }
+
+    while (idx2 <= last) {
+        temp[k++] = arr[idx2++];
+    }
+
+    for (let i = 0, j = first; i < temp.length; i++, j++) {
+        arr[j] = temp[i];
+    }
 }
