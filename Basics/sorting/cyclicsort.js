420a8d813e356f6d8f6079881e3873d8d537fdd5
prafulpawar <prafuldukhi@gmail.com>
2025-02-11 22:36:43 +0530

    _Cyclic_Sort_Done

--- Basics/sorting/cyclicsort.js
+++ Basics/sorting/cyclicsort.js
@@ -1,5 +1,13 @@
 let arr = [5,6,7,8,9,1,2,3,5,7];
 
-for(let i=0;i<arr.length-1;i++){
-     if(arr[i]  )
+let i =0
+while(i<arr.length){
+      let correctIndex = arr[i]-1;
+      if(arr[i]!=arr[correctIndex]){
+         let temp = arr[i];
+         arr[i] = arr[correctIndex];
+         arr[correctIndex] = temp
+      }
+      else i++
 }
+console.log(arr)
