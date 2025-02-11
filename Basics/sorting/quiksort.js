bee8e7e27bad4060aebd8d969f27c865a89078eb
prafulpawar <prafuldukhi@gmail.com>
2025-02-11 11:13:11 +0530

    __Done_Quike_Sort

--- Basics/sorting/quiksort.js
+++ Basics/sorting/quiksort.js
@@ -1,7 +1,33 @@
-let arr  = [7,4,3,1,5,4,9]
+let arr  = [7,4,3,1,5,4,9];
 
-quikeSort(arr,0,arr.length-1);
+quickSort(arr, 0, arr.length - 1);
 
-function quikeSort (arr,first,last){
-        let pivot = arr[last],i=first-1;
+console.log(arr); // Sorted array output
+
+function quickSort(arr, first, last) {
+    if (first < last) {
+        let pivotIdx  = partition(arr, first, last);
+        quickSort(arr, first, pivotIdx - 1);
+        quickSort(arr, pivotIdx + 1, last);
+    }
+}
+
+function partition(arr, first, last) {
+    let pivot = arr[last], i = first - 1;
+    for (let j = first; j < last; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    i++;
+    swap(arr, i, last);
+    return i;
+}
+
+// Swap function to swap two elements in the array
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
