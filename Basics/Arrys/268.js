a3ac84ad18e901b3dda71dfc227288ccdba30efe
prafulpawar <prafuldukhi@gmail.com>
2025-02-12 12:15:27 +0530

    done_448

--- Basics/Arrys/268.js
+++ Basics/Arrys/268.js
@@ -3,35 +3,57 @@
  * @return {number}
  */
 
-var missingNumber = function(nums) {
-//     let i =0
-// while(i<nums.length){
-//       let correctIndex = nums[i];
-//       if(nums[i]!=nums[correctIndex]){
-//          let temp = nums[i];
-//          nums[i] = nums[correctIndex];
-//          nums[correctIndex] = temp
-//       }
-//       else i++
-// }
-// let k = nums.length;
-// for(let i =0;i<nums.length;i++){
-//       if(nums[i] === undefined){
-//             k=i
-//       }
-// }
-// return i
+// var missingNumber = function(nums) {
+// //     let i =0
+// // while(i<nums.length){
+// //       let correctIndex = nums[i];
+// //       if(nums[i]!=nums[correctIndex]){
+// //          let temp = nums[i];
+// //          nums[i] = nums[correctIndex];
+// //          nums[correctIndex] = temp
+// //       }
+// //       else i++
+// // }
+// // let k = nums.length;
+// // for(let i =0;i<nums.length;i++){
+// //       if(nums[i] === undefined){
+// //             k=i
+// //       }
+// // }
+// // return i
    
-  let i =0;
-  while()
+// }
 
 
-
-
-
-
-
-};
+function missingNumber(nums) {
+      let i = 0;
+      while (i < nums.length) {
+          let correct = nums[i]; 
+  
+          if (nums[i] < nums.length && nums[i] !== nums[correct]) {
+              // Swap nums[i] and nums[correct]
+              let temp = nums[i];
+              nums[i] = nums[correct];
+              nums[correct] = temp;
+          } else {
+              i++;
+          }
+      }
+  
+      for (let j = 0; j < nums.length; j++) {
+          if (nums[j] !== j) {
+              return j; // Missing number found
+          }
+      }
+  
+      return nums.length; // If all numbers are correct, return the last missing number
+  }
+  
+  // Test case
+  let nums = [0, 1];
+  let result = missingNumber(nums);
+  console.log(result); // Expected output: 2
+  
 
 // 
 
--- Basics/Arrys/448.js
+++ Basics/Arrys/448.js
@@ -6,23 +6,24 @@ nums = [4,3,2,7,8,2,3,1];
 var findDisappearedNumbers = function (nums) {
     let i = 0
     while (i < nums.length) {
-        let correctIndex = nums[i];
-        if (nums[i] != nums[correctIndex]) {
+        let correct = nums[i]-1;
+        if (nums[i] != nums[correct]) {
             let temp = nums[i];
-            nums[i] = nums[correctIndex];
-            nums[correctIndex] = temp
+            nums[i] = nums[correct];
+            nums[correct] = temp
         }
         else i++
     }
 
 
-    let k = nums.length;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === undefined) {
-            k = i
+    let ans = [];
+    for (let j = 0; j < nums.length; j++) {
+        if(j+1!=nums[j]){
+            ans.push(j+1)
         }
     }
-    return i
+    return ans
 };
 
-let x = findDisappearedNumbers()
+let x = findDisappearedNumbers(nums)
+console.log(x)
