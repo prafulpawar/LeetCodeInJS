cd42588925febb8889d44a7593cffc751c082981
prafulpawar <prafuldukhi@gmail.com>
2025-02-12 22:05:57 +0530

    __Done_Cyclic_Sort

--- Basics/sorting/cyclicsort.js
+++ Basics/sorting/cyclicsort.js
@@ -1,16 +1,15 @@
 let arr = [9,6,4,2,3,5,7,8,1]
 let i =0
 while(i<arr.length){
-      let correctIndex = arr[i]-1;
-      
-      if(arr[i]!=arr[correctIndex]){
-            
-         let temp = arr[i];
-         
-         arr[i] = arr[correctIndex];
-         arr[correctIndex] = temp
-      }
-      else i++
+       let index = arr[i]-1;
+       if(arr[i]!= arr[index]){
+            temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+       }
+       else{
+            i++
+       }
       
 }
 console.log(arr)
