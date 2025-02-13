1f041aa786358c28c570dad1b1be42edf94e7a31
prafulpawar <prafuldukhi@gmail.com>
2025-02-13 22:21:29 +0530

    __34_done

--- Basics/sorting/binary/34.js
+++ Basics/sorting/binary/34.js
@@ -5,33 +5,50 @@
  */
 var searchRange = function(nums, target) {
    
-    let newArr = []
-    let start = 0, end = nums.length - 1;
-    
-    while (start <= end) {
-      
-        let mid = Math.floor((start + end) / 2);
+  function lastIndex(nums, target) {
+      let start = 0, end = nums.length - 1;
+      let p = -1; 
+      while (start <= end) {
+          let mid = Math.floor((start + end) / 2);
 
-        
-        if (nums[mid] === target) {
-           
-          return  newArr.push(mid)
-        
-        } 
+          if (nums[mid] === target) {
+              p = mid;  
+              start = mid + 1;  
+          } else if (nums[mid] < target) {
+              start = mid + 1;
+          } else {
+              end = mid - 1;
+          }
+      }
+      return p;
+  }
 
-        else if (nums[mid] < target  )
-            start = mid + 1;
-        else
-            end = mid - 1;
-    }
-     console.log(newArr)
-   if(newArr.length<0){
-      return [-1,-1]
-   }
-   else{
-    return newArr
-   }
+  function firstIndex(nums, target) {
+      let start = 0, end = nums.length - 1;
+      let p = -1;
+      while (start <= end) {
+          let mid = Math.floor((start + end) / 2);
+
+          if (nums[mid] === target) {
+              p = mid;  
+              end = mid - 1;  
+          } else if (nums[mid] < target) {
+              start = mid + 1;
+          } else {
+              end = mid - 1;
+          }
+      }
+      return p;
+  }
+
+  let first = firstIndex(nums, target);
+  let last = lastIndex(nums, target);
+
+  return [first, last];
 };
 
-let x  = searchRange([5,7,7,8,8,10],8)
-console.log(x)
+// Test cases
+console.log(searchRange([5,7,7,8,8,8,10], 7));  // Output: [1,2]
+console.log(searchRange([5,7,7,8,8,8,10], 8));  // Output: [3,5]
+console.log(searchRange([5,7,7,8,8,8,10], 6));  // Output: [-1,-1]
+console.log(searchRange([], 0));  // Output: [-1,-1]
