6b5abe2eb4ea349629dbc1894ec635e85613061c
prafulpawar <prafuldukhi@gmail.com>
2025-02-15 22:54:48 +0530

    __367_Done

--- Basics/sorting/binary/367.js
+++ Basics/sorting/binary/367.js
@@ -2,8 +2,28 @@
  * @param {number} num
  * @return {boolean}
  */
+
+
+
 var isPerfectSquare = function(num) {
-    
+    let start = 0, end = Math.floor(num/2);
+
+    while (start <= end) {
+
+      
+        let mid = Math.floor((start + end) / 2);
+
+        
+        if (arr[mid] === x) return true;
+
+     
+        else if (arr[mid] < x)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+
+    return false;
 };
 let p = 16
 const x = isPerfectSquare(p)
