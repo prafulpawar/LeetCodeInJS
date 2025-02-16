49aaee6ea73946309e86a74af054b0de8a9ae717
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 16:40:25 +0530

    __Done744

--- Basics/sorting/binary/744.js
+++ Basics/sorting/binary/744.js
@@ -5,6 +5,29 @@
  */
 var nextGreatestLetter = function(letters, target) {
     
+
+    let start = 0, end = letters.length - 1;
+    let pos = -1
+    while (start <= end) {
+
+      
+        let mid = Math.floor((start + end) / 2);
+        console.log(mid)
+
+     
+         if (letters[mid] > target){
+            pos = letters[mid]
+            end = mid - 1;
+         }
+           
+        else
+            start = mid + 1;
+    }
+
+    if(pos ==-1){
+        return letters[0]
+    }
+    return pos
 };
 
 
