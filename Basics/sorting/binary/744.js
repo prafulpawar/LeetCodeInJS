6dec1ab924ca542ed5b9cda6802904371fa70f2d
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 16:09:23 +0530

    744_Added

---
+++ Basics/sorting/binary/744.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    
+};
+
+
+let letters = ["c","f","j"], target = "a"
+
+const x = nextGreatestLetter(letters,target);
+console.log(x)
