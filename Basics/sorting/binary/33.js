9c2462845b808f5fedb1c29c6c23b3f0f47f6eaa
prafulpawar <prafuldukhi@gmail.com>
2025-02-17 13:48:20 +0530

    done__33

--- Basics/sorting/binary/33.js
+++ Basics/sorting/binary/33.js
@@ -5,18 +5,34 @@
  */
 var search = function(nums, target) {
     let l = 0;
-    let r  = nums.target-1;
-     while(l<=r){
+    let r = nums.length - 1;
+
+    while (l <= r) {
         let mid = Math.floor((l + r) / 2);
 
-        if (nums[mid] === target) {
-            p = mid;  
-            start = mid + 1;  
-        } else if (nums[mid] < target) {
-            l = mid + 1;
-        } else {
-            r = mid - 1;
+        if (nums[mid] === target) return mid;
+
+        // Left half is sorted
+        if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) {
+                r = mid - 1; // Move left
+            } else {
+                l = mid + 1; // Move right
+            }
+        } 
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[r]) {
+                l = mid + 1; // Move right
+            } else {
+                r = mid - 1; // Move left
+            }
         }
-     }
+    }
+    return -1; // Target not found
     
 };
+
+let nums = [4,5,6,7,0,1,2], target = 0
+let x = search(nums,target)
+console.log(x)
