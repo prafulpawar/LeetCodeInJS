0fd2a74762570f3a79feb85a0cf912ef4f796f86
prafulpawar <prafuldukhi@gmail.com>
2025-02-17 11:47:09 +0530

    __33 added

--- Basics/sorting/binary/33.js
+++ Basics/sorting/binary/33.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
+    let l = 0;
+    let r  = nums.target-1;
+     while(l<=r){
+        let mid = Math.floor((l + r) / 2);
+
+        if (nums[mid] === target) {
+            p = mid;  
+            start = mid + 1;  
+        } else if (nums[mid] < target) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+     }
     
 };
