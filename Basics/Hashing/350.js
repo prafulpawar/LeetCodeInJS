82bc7da30400d2a79e7e6b9f344566ed5d60236b
prafulpawar <prafuldukhi@gmail.com>
2025-02-19 14:41:06 +0530

    __Done350&&409

--- Basics/Hashing/350.js
+++ Basics/Hashing/350.js
@@ -3,23 +3,25 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function(nums1, nums2) {
+var intersect = function (nums1, nums2) {
     let map = new Map();
     let ans = [];
-for (let i = 0; i <nums1.length; i++) {
-    if (map.has(nums1[i])) {
-        map.set(nums1[i], map.get(nums1[i]) + 1);
-    } else {
-        map.set(nums1[i], 1);
+
+    for (let i = 0; i < nums1.length; i++) {
+        map.set(nums1[i], (map.get(nums1[i]) || 0) + 1);
     }
-}
-  return map;
-     
+
+    for (let i = 0; i < nums2.length; i++) {
+        if (map.has(nums2[i]) && map.get(nums2[i]) > 0) {
+            ans.push(nums2[i]);
+            map.set(nums2[i], map.get(nums2[i]) - 1);
+        }
+    }
+
+    return ans;
 };
 
+let nums1 = [1, 2, 2, 1], nums2 = [2, 2];
 
-let  nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-
-
-let x  = intersect(nums1,nums2)
-console.log(x);
+let x = intersect(nums1, nums2);
+console.log(x); 
