d30234d7264b42b271a67026d16b55a7f505df7d
prafulpawar <prafuldukhi@gmail.com>
2025-02-19 14:51:51 +0530

    _Done__409

---
+++ Basics/Hashing/409.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let map = new Map();
+    let ans = [];
+
+    for (let i = 0; i <s.length; i++) {
+        map.set(s[i], (map.get(s[i]) || 0) + 1);
+    }
+  console.log(map)
+};
+
+let  s = "abccccdd"
+let x  = longestPalindrome(s);
+console.log(x)
