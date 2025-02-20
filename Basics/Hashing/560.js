11f32d9b4c8de8a3e55bef6c53a18ac8c3b52804
prafulpawar <prafuldukhi@gmail.com>
2025-02-20 12:20:51 +0530

    __Done_560

--- Basics/Hashing/560.js
+++ Basics/Hashing/560.js
@@ -4,23 +4,25 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let map = new Map();
-    for (let values of nums) {
-        map.set(values, (map.get(values) || 0) + 1);
-    }
-     console.log(map)
     let sum = 0;
     let count = 0;
-    let i =0;
-    for (const [key, value] of map.entries()) {
-          if(key === k){
-             sum=sum+1;
-          }
-          else{
-               map.set(nums[i],(map.get(i) ||0 +1))
-          }
-      }
-      return sum;
+    let map = new Map();
+    map.set(0, 1);
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        let rem = sum - k;
+
+        if (map.has(rem)) {
+            console.log(rem)
+            
+            count += map.get(rem);
+        }
+
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
+
+    return count;
 };
 
 
