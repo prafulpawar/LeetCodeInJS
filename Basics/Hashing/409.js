82555a84306a1ac860918900dc0448d45e5bcd8c
prafulpawar <prafuldukhi@gmail.com>
2025-02-20 11:01:38 +0530

    409_Done

--- Basics/Hashing/409.js
+++ Basics/Hashing/409.js
@@ -3,15 +3,32 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    let map = new Map();
-
-    for (let i = 0; i <s.length; i++) {
-        map.set(s[i], (map.get(s[i]) || 0) + 1);
+    let charCount = new Map();
+    for (let char of s) {
+        charCount.set(char, (charCount.get(char) || 0) + 1);
     }
-    console.log(map)
-
+    
+    let maxLength = 0;
+    let hasOdd = false;
+    
+    for (let count of charCount.values()) {
+        if (count % 2 === 0) {
+            maxLength += count;
+        } else {
+            maxLength += count - 1;
+            hasOdd = true;
+        }
+    }
+    
+    return hasOdd ? maxLength + 1 : maxLength;
 };
 
 let  s = "abccccdd"
 let x  = longestPalindrome(s);
 console.log(x)
+
+// Count Frequencies: Use a hashmap to count occurrences of each character.
+// Build the Palindrome:
+// Any character that appears an even number of times can be fully used.
+// If a character appears an odd number of times, we can use the even part of it.
+// We can add one odd character in the center of the palindrome.
