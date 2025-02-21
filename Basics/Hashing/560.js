605e65b36b95a80d3b3e02ecd74a507486082c98
prafulpawar <prafuldukhi@gmail.com>
2025-02-21 13:45:49 +0530

    __Done__560

--- Basics/Hashing/560.js
+++ Basics/Hashing/560.js
@@ -3,27 +3,46 @@
  * @param {number} k
  * @return {number}
  */
-var subarraySum = function(nums, k) {
-    let sum = 0;
+var subarraySum = function (nums, k) {
+    // let sum = 0;
+    // let count = 0;
+    // let map = new Map();
+    // map.set(0, 1);
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     sum += nums[i];
+    //     let rem = sum - k;
+
+    //     if (map.has(rem)) {
+    //         count += map.get(rem);
+    //     }
+
+    //     map.set(sum, (map.get(sum) || 0) + 1);
+    // }
+
+    // return count;
+
+    //  Brut Force Solution
     let count = 0;
-    let map = new Map();
-    map.set(0, 1);
-
     for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
-        let rem = sum - k;
-
-        if (map.has(rem)) {
-            count += map.get(rem);
+        let subArray = []; 
+        for (let j = i; j < nums.length; j++) {
+            subArray.push(nums[j]);
+              let sum =0;
+               for(let k=0; k<subArray.length;k++){
+                   sum=sum+nums[k]
+               }
+               if(sum===k){
+                count++;
+               }
+               console.log(subArray,' ',sum)
         }
-
-        map.set(sum, (map.get(sum) || 0) + 1);
     }
-
-    return count;
+   return count
 };
 
 
-let nums = [9, 4, 20, 3, 10, 5], k = 33;
-let ans =subarraySum(nums,k)
+let nums = [-1,-1,1], k = 0;
+let ans = subarraySum(nums, k)
 console.log(ans)
+
