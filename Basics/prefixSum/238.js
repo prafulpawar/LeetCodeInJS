6a3582791a8844829bd173a6e7c3b18c23f9e49f
prafulpawar <prafuldukhi@gmail.com>
2025-02-23 21:16:07 +0530

    __Done

---
+++ Basics/prefixSum/238.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+};
+
+let nums = [1,2,3,4]
+let x = productExceptSelf(nums)
+console.log(x)
