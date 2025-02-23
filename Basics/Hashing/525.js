d1f8fdd5bad8503cae66e81862256dba473034fc
prafulpawar <prafuldukhi@gmail.com>
2025-02-23 21:12:42 +0530

    __Done

--- Basics/Hashing/525.js
+++ Basics/Hashing/525.js
@@ -6,17 +6,17 @@ var findMaxLength = function(nums) {
     let sum = 0;
     let maxLength = 0;
     let map = new Map();
-    map.set(0, -1); // Initialize with sum 0 at index -1
+    map.set(0, -1); 
     
     for (let i = 0; i < nums.length; i++) {
-        // Convert 0 to -1 for balanced sum calculation
+       
         sum += nums[i] === 0 ? -1 : 1;
         
-        // If sum exists in map, calculate subarray length
+   
         if (map.has(sum)) {
             maxLength = Math.max(maxLength, i - map.get(sum));
         } else {
-            // Store first occurrence of the sum
+           
             map.set(sum, i);
         }
     }
