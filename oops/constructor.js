27a35db3a6fe86ab7d8c732621a99b63478633a9
prafulpawar <prafuldukhi@gmail.com>
2025-02-24 12:33:00 +0530

    __Done

--- oops/constructor.js
+++ oops/constructor.js
@@ -3,16 +3,21 @@
 // js me class ke naam se constructor nhi hota hai sirf vo constructor naam se hota hai
 // now calling 
 // this matlb ki current calling object
+// objects ko hum instance bolte hai or uskei propertty methods ko hum behaviour bhi bol sakte hai
 class Student{
      constructor(name,age){
         this.name = name;
         this.age  = age
-        console.log("Running Constructor",+' '+name+' '+age)
+        console.log("Running Constructor"+' '+ name+' '+age)
+     }
+     fun(){
+        console.log("FUN HO RAHA HAI")
      }
 }
 const data  = new Student("ABCD",35);
 const data1  = new Student("DSAX",75);
 const data2 = new Student("QWER",75);
-const data3  = new Student("ABCD",45);
+const data3  = new Student("MUCS",45);
 
-console.log(data,data1,data2,data3)
+// console.log(data,data1,data2,data3)
+console.log(data.fun())
