d78a71a366d5ae0ca826cfc19e813fc2b3f183c5
prafulpawar <prafuldukhi@gmail.com>
2025-02-24 12:27:14 +0530

    __Done__Oops

--- oops/constructor.js
+++ oops/constructor.js
@@ -2,9 +2,11 @@
 // class me ap define karo ya na karo but ek default constructor hota hai
 // js me class ke naam se constructor nhi hota hai sirf vo constructor naam se hota hai
 // now calling 
+// this matlb ki current calling object
 class Student{
      constructor(name,age){
-        console.log(name,age)
+        this.name = name;
+        this.age  = age
         console.log("Running Constructor")
      }
 }
