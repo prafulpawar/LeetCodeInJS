f71f85b811f55a7b2f297a96adde45792a0ea33c
prafulpawar <prafuldukhi@gmail.com>
2025-02-24 12:15:00 +0530

    __Done

---
+++ oops/1.js
@@ -0,0 +1,15 @@
+class Student{
+   
+}
+const data  = new Student();
+data.name   = "ABCD"
+data.age    = 35
+data.email  = "abcd@gmail.com"
+
+const data2  = new Student();
+data2.name   = "DSAQ"
+data2.age    = 49
+data2.email  = "DSAQ@gmail.com"
+
+console.log(data)
+console.log(data2)
