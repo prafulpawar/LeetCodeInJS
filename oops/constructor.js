e80e0d6933eaf85fa8bd1ec68f9400eb75b22bde
prafulpawar <prafuldukhi@gmail.com>
2025-02-24 12:24:37 +0530

    done

--- oops/constructor.js
+++ oops/constructor.js
@@ -1,18 +1,14 @@
 // now ab role me aayenga constructor
 // class me ap define karo ya na karo but ek default constructor hota hai
 // js me class ke naam se constructor nhi hota hai sirf vo constructor naam se hota hai
-
+// now calling 
 class Student{
-     constructor(){
+     constructor(name,age){
+        console.log(name,age)
         console.log("Running Constructor")
      }
 }
-const data  = new Student();
-data.name   = "ABCD"
-data.age    = 35
-data.email  = "abcd@gmail.com"
-
+const data  = new Student("ABCD",25);
 
 
 console.log(data)
-console.log(data2) 
