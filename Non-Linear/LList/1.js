8a799109120e29690329601236aa98df202ad0c8
prafulpawar <prafuldukhi@gmail.com>
2025-02-25 14:47:35 +0530

    __Done

--- Non-Linear/LList/1.js
+++ Non-Linear/LList/1.js
@@ -1,22 +1,45 @@
-class Node{
-     constructor(){
-         this.val = val;
-         this.next = null;
-     }
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
-class LinkedList{
-    constructor(){
-        this.head = null
+class LinkedList {
+    constructor() {
+        this.head = null;
     }
-    insertAtFirst(val){
+
+    insertAtFirst(val) {
         let newNode = new Node(val);
-        if(this.head = null){
-            this.head =newNode
-            return 
+        if (this.head === null) {
+            this.head = newNode;
+            return;
         }
-        newNode.next = this.head
-        this.head = newNode
+        newNode.next = this.head;
+        this.head = newNode;
+    }
 
+    print() {
+        if (this.head === null) {
+            console.log("No Node Found!!!!!");
+            return;
+        }
+        let temp = this.head;
+        let ans = [];
+        while (temp !== null) {
+            ans.push(temp.val);  
+            temp = temp.next;
+        }
+        console.log(ans.join(" -> "));
     }
 }
+
+let obj = new LinkedList();
+obj.insertAtFirst(10);
+obj.insertAtFirst(20);
+obj.insertAtFirst(30);
+obj.insertAtFirst(40);
+obj.insertAtFirst(50);
+
+obj.print();
