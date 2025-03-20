c4093642cf469a8a3393317b3f342831c27ffe54
prafulpawar <prafuldukhi@gmail.com>
2025-03-21 01:58:10 +0530

    __DONE__

--- Non-Linear/LList/list.js
+++ Non-Linear/LList/list.js
@@ -17,16 +17,35 @@ class LinkedList {
     }
 
     insertAtLast(val){
-        let newNode = new Node(val) // ek new node banai hamane
 
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+
+        let newNode = new Node(val) // ek new node banai hamane
+        let temp = this.head
+       while(temp.next !=null){
+           temp = temp.next;
+       }
+       temp.next = newNode
+        
     }
 
+    insertAtSpeciic(val , pos) {
+       if(this.head === null){
+        return
+       }
 
+
+
+
+    }
 
     print(){
         let temp = this.head;
         while(temp !== null){
-             console.log(temp.val)
+              console.log(temp.val)
             temp = temp.next;
         }
     }
@@ -38,7 +57,16 @@ const obj = new LinkedList();
 obj.insertAtFirst(10)
 obj.insertAtFirst(20)
 obj.insertAtFirst(30)
-obj.print()
-console.log('-----------');
+// obj.print()
+// console.log('-----------');
 
+obj.insertAtLast(40)
+obj.insertAtLast(50)
+obj.insertAtLast(60)
+
+/// Insert At Specifc
+
+obj.insertAtSpeciic(70,)
+
+obj.print()
 
