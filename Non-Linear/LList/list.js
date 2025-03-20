b49e0199d56307d2533527f6ed7b6552457fa0cb
prafulpawar <prafuldukhi@gmail.com>
2025-03-20 18:42:13 +0530

    __Done__

--- Non-Linear/LList/list.js
+++ Non-Linear/LList/list.js
@@ -3,8 +3,12 @@ function node(data){
     this.next = null;
 }
 
-var head = new node(5);
-var mid  = new node(10);
+var head  = new node(5);
+var mid   = new node(10);
+var tail  = new node(13)
+
+head.next = mid; // refernce to next node
+mid.next  = tail
 
 
 console.log(head)
