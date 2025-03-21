a4261d401a6b84ccc8ddab4f106689dfdd28d587
prafulpawar <prafuldukhi@gmail.com>
2025-03-21 08:42:14 +0530

    __DONE__

--- Non-Linear/LList/list.js
+++ Non-Linear/LList/list.js
@@ -32,20 +32,48 @@ class LinkedList {
         
     }
 
-    insertAtSpeciic(val , pos) {
-       if(this.head === null){
-        return
-       }
-
-
-
-
+    insertAtSpecific(val, pos) {
+        if (pos < 0) {
+            console.log("Invalid position! Position must be >= 0.");
+            return false;
+        }
+    
+        const newNode = new Node(val);
+    
+        // Insert at beginning
+        if (pos === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+            return true;
+        }
+    
+        let temp = this.head;
+        let i = 0;
+    
+        // Traverse to the node before `pos`
+        while (temp !== null && i < pos - 1) {
+            temp = temp.next;
+            i++;
+        }
+    
+        // If `temp` is null, pos is out of range
+        if (temp === null) {
+            console.log("Position out of range!");
+            return false;
+        }
+    
+        // Insert new node at position
+        newNode.next = temp.next;
+        temp.next = newNode;
+        
+        return true;
     }
+    
 
     print(){
         let temp = this.head;
         while(temp !== null){
-              console.log(temp.val)
+               console.log(temp.val)
             temp = temp.next;
         }
     }
@@ -54,9 +82,9 @@ class LinkedList {
 }
 
 const obj = new LinkedList();
-obj.insertAtFirst(10)
-obj.insertAtFirst(20)
 obj.insertAtFirst(30)
+obj.insertAtFirst(20)
+obj.insertAtFirst(10)
 // obj.print()
 // console.log('-----------');
 
@@ -66,7 +94,7 @@ obj.insertAtLast(60)
 
 /// Insert At Specifc
 
-obj.insertAtSpeciic(70,)
+obj.insertAtSpeciic(70,4)
 
 obj.print()
 
