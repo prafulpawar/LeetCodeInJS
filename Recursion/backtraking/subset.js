d6fef6a64d052e4dd869a98d307455db53948f34
prafulpawar <prafuldukhi@gmail.com>
2025-04-01 23:10:22 +0530

    __done__

--- Recursion/backtraking/subset.js
+++ Recursion/backtraking/subset.js
@@ -1,10 +1,24 @@
-function subsets(nums){
+function subsets(nums) {
     let result = [];
     let temp = [];
 
-    function subSets(nums,i){
-       
+    function subSets(i) {
+        if (i === nums.length) {
+            result.push([...temp]);
+            return;
+        }
+
+        // Include the current element
+        temp.push(nums[i]);
+        subSets(i + 1);
+
+        // Exclude the current element
+        temp.pop();
+        subSets(i + 1);
     }
-    subSets(nums,i);
-    return result
+
+    subSets(0);
+    return result;
 }
+
+console.log(subsets([1,2,3,4,5]));
