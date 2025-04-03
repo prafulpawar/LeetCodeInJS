2e43e5b490c123b4828c4d8cb2ae17002910d5ce
prafulpawar <prafuldukhi@gmail.com>
2025-04-03 11:31:01 +0530

    __DONE__

--- Non-Linear/Tree/tree.js
+++ Non-Linear/Tree/tree.js
@@ -1,3 +1,4 @@
+const prompt = require('prompt-sync')();
 class Node{
     constructor(val){
         this.val = val;
@@ -5,3 +6,23 @@ class Node{
         this.rigth = null;
     }
 }
+
+class Tree{
+    constructor(){
+        this.root = null;
+    }
+    createTree(){
+        let data = prompt("Enter Value");
+        if(data ===-1){
+            return null;
+        }
+        this.root = new Node(data);
+        console.log('Enter Left Child For ',data);
+        this.root.left = this.createTree();
+        console.log('Enter Right Child For',data);
+        this.root.rigth = this.createTree();
+    }
+}
+
+let obj = new Tree();
+obj.createTree()
