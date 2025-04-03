7cd55055d8e588de1af1a1be738e59ed8f767140
prafulpawar <prafuldukhi@gmail.com>
2025-04-03 11:58:25 +0530

    __DONE__

--- Non-Linear/Tree/tree.js
+++ Non-Linear/Tree/tree.js
@@ -1,29 +1,69 @@
 const prompt = require('prompt-sync')();
-class Node{
-    constructor(val){
+
+class Node {
+    constructor(val) {
         this.val = val;
         this.left = null;
-        this.rigth = null;
+        this.right = null;  
     }
 }
-class Tree{
-    constructor(){
+
+class Tree {
+    constructor() {
         this.root = null;
     }
-    createTree(){
-        let data = prompt("Enter Value");
-        if(data ===-1){
+
+    createTree() {
+        let data = prompt("Enter Value: ");
+        if (parseInt(data) === -1) {  
             return null;
         }
-        this.root = new Node(data);
-        console.log('Enter Left Child For ',data);
-        this.root.left = this.createTree();
-        console.log('Enter Right Child For',data);
-        this.root.rigth = this.createTree();
+
+        let node = new Node(parseInt(data));
+        console.log('Enter Left Child For', node.val);
+        node.left = this.createTree();
+        console.log('Enter Right Child For', node.val);
+        node.right = this.createTree(); 
+        return node;
+    }
+
+    preorder(root) {
+        if (root === null) {
+            return;
+        }
+
+        process.stdout.write(root.val + " ");  
+        this.preorder(root.left);
+        this.preorder(root.right);
+    }
+
+    postorder(root) {
+        if (root === null) {
+            return;
+        }
+
+        this.postorder(root.left);
+        this.postorder(root.right);
+        process.stdout.write(root.val + " ");  
+    }
+
+    inorder(root) {
+        if (root === null) {
+            return;
+        }
+
+        this.inorder(root.left);
+        process.stdout.write(root.val + " ");  
+        this.inorder(root.right);
+       
     }
 }
 
 let obj = new Tree();
-obj.createTree()
+obj.root = obj.createTree();
 
-// Tree Travelsal
+console.log("Preorder Traversal:");
+obj.preorder(obj.root);
+
+console.log("Postorder Traversal:");
+obj.postorder(obj.root)
