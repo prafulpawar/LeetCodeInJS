5778a2be24ac402180bfc698b647762dc038fd6b
prafulpawar <prafuldukhi@gmail.com>
2025-04-25 19:30:42 +0530

    __Done__

--- Linear/Arrys/rorateBy1.js
+++ Linear/Arrys/rorateBy1.js
@@ -1,3 +1,9 @@
 let arr = [10,20,30,40,50];
 //  Op Be Like ----> [20,30,40,50,10]
+let copy = arr[0];
 
+for(let i =0;i<arr.length;i++){
+     arr[i] = arr[i+1];     
+}
+arr[arr.length-1] = copy;
+console.log(arr)
