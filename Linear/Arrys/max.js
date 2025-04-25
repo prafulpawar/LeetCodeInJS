c8f43e31a56cf45e2186ab1b00fc696a44cbde41
prafulpawar <prafuldukhi@gmail.com>
2025-04-25 19:06:47 +0530

    __DONE__

---
+++ Linear/Arrys/max.js
@@ -0,0 +1,18 @@
+let arr = [1,2,3,4,5];
+let max = 0;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>max){
+        max=arr[i];
+    }
+}
+
+console.log(max)
+
+// Second Max 
+// Approch It Shuld Be Less Then Great An Great Then 
+let m =arr[0];
+let s =0;
+
+for(let i=0;i<arr.length;i++){
+     if()
+}
