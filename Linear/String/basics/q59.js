f11bd56166bf8c3543e4885f98c834b822e76e6c
prafulpawar <prafuldukhi@gmail.com>
2025-04-26 19:13:52 +0530

    __DONE__

--- Linear/String/basics/q59.js
+++ Linear/String/basics/q59.js
@@ -1,7 +1,7 @@
 let  words = ["pay","attention","practice","attend"];
-pref = "al"
+pref = "att"
 for(let i=0;i<words.length-1;i++){
-    if(words[i].includes(pref)){
+    if(words[i].startsWith(pref)){
         console.log(i+1)
         return 
     }
