6b667ff4ff488d8a7dc30b7a3d6394471492f421
prafulpawar <prafuldukhi@gmail.com>
2025-04-26 16:52:43 +0530

    __DONE__

--- Linear/String/basics/q62.js
+++ Linear/String/basics/q62.js
@@ -1,10 +1,20 @@
-let w1 = car;
-let w2 = arc;
+let w1 = 'state';
+let w2 = 'taste';
 
 if(w1.length!=w2.length){
     console.log(false);
     return;
 }
 
-let freq1 = {};
-let freq2 = {};
+let set = new Set(w2);
+
+
+for(let i=0;i<w1.length;i++){
+     let x = w1[i];
+     if(!set.has(x)){
+         console.log(false)
+         return;
+     }
+
+}
+console.log(true)
