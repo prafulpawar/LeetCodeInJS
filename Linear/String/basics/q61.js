8dbad9396d9829763a3f3edc1a240760798dc35f
prafulpawar <prafuldukhi@gmail.com>
2025-04-26 18:42:04 +0530

    __DONE__

--- Linear/String/basics/q61.js
+++ Linear/String/basics/q61.js
@@ -8,3 +8,9 @@ for(let char of str){
      }
 }
 console.log(freq)
+
+//  const frequencyMap = {};
+//   for (const element of arr) {
+//     frequencyMap[element] = (frequencyMap[element] || 0) + 1;
+//   }
+//   return frequencyMap;
