de7f2ee46710d40429e78b202881a611140ff96f
prafulpawar <prafuldukhi@gmail.com>
2025-04-26 19:25:42 +0530

    __DONE__

--- Linear/String/basics/q60.js
+++ Linear/String/basics/q60.js
@@ -2,9 +2,13 @@ let word = 'Hello bhai Kaise ho a';
 let op = '';
 
 for (let i = 0; i < word.length; i++) {
-    if (i === 0 || word[i - 1] === ' ') {
+    if (i === 0 ) {
         op += word[i].toUpperCase();
-    } else {
+    }
+    else if(word[i - 1] === ' '){
+        op += word[i].toUpperCase();
+    }
+    else {
         op += word[i];
     }
 }
