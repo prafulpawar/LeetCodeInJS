9c7ddbecefa54a83656b72af9e4c507923250a90
prafulpawar <prafuldukhi@gmail.com>
2025-04-28 19:22:13 +0530

    __DONE__

--- session16-17/map/freq.js
+++ session16-17/map/freq.js
@@ -8,3 +8,15 @@ function frequencyCounter(arr) {
 }
 let ans = frequencyCounter(arr);
 console.log(ans)
+
+
+// Another Way
+let map = new Map();
+for(let i=0;i<arr.length;i++){
+    if(map.has(arr[i])){
+        map.set(arr[i],map.get(arr[i])+1)
+    }
+    else{
+        map.set(arr[i],i);
+    }
+}
