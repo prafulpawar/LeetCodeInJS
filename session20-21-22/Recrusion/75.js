645caf941d8e2a9e8be631be6798ee6ef3a18031
prafulpawar <prafuldukhi@gmail.com>
2025-04-29 17:26:23 +0530

    __DONE__

--- session20-21-22/Recrusion/75.js
+++ session20-21-22/Recrusion/75.js
@@ -1,5 +1,13 @@
-function fact (n){
-
+function fact (n,data){
+     if (n===0){
+        return 1;
+     }
+    let a = fact(n-1,data);
+    console.log(a)
+    return (a+1);
 }
 
 let n = 5;
+let data = 1;
+let ans = fact(n,data);
+// console.log(ans)
