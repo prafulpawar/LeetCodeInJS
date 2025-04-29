2c67919fc2e48fbf17d7fd793be082f93641822e
prafulpawar <prafuldukhi@gmail.com>
2025-04-29 17:10:15 +0530

    __DONE__

--- session20-21-22/Recrusion/74.js
+++ session20-21-22/Recrusion/74.js
@@ -1,6 +1,25 @@
+function funcN (n) {
+    if(n==0){
+        return;
+    }
+    console.log(n)
+    funcN(n-1);
+}
+
+
+// - - n
 function func (n) {
+    if(n==0){
+        return ;
+    }
     
-    func(n);
+     func(n-1);
+    console.log(n)
+   
+    
 }
 
 let a = 5;
+func(a);
+let b = 10;
+funcN(b)
